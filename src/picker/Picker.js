8a64ad535a6ff9889d3d6aa882147be12702ccce
Ian Robinson <ianrob1201@gmail.com>
2018-06-10 21:50:28 +0100

    Picker improvements

--- src/picker/Picker.js
+++ src/picker/Picker.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import { browserHistory } from 'react-router';
+import './picker.css';
 
 export default class Picker extends Component {
 	constructor (props) {
@@ -12,6 +14,7 @@ export default class Picker extends Component {
 		this.handleUrlChange = this.handleUrlChange.bind(this);
 		this.handleSprintChange = this.handleSprintChange.bind(this);
 		this.showBoard = this.showBoard.bind(this);
+		this._handleKeyPress = this._handleKeyPress.bind(this);
 	}
 
 	handleUrlChange(event) {
@@ -22,22 +25,38 @@ export default class Picker extends Component {
 		this.setState({sprintId: event.target.value});
 	}
 
-	showBoard(event) {
+	showBoard() {
+		// replaceState({ nextPathname: nextState.location.pathname }, '/login')
+		this.props.history.push('/board?url=' + this.state.url + '&sprintId=' + this.state.sprintId);
+	}
 
+	_handleKeyPress(event) {
+		if (event.key === 'Enter') {
+     		this.showBoard();
+    	}
 	}
 
 	render() {
 		return (
-			<div>
+			<div className="picker">
 				<h1>JIRA Details</h1>
 				<div className="input">
 					<p>URL:</p>
-					<input type="text" value={this.state.url} onChange={this.handleUrlChange} id="jira-url"/>
+					<input type="text" 
+						value={this.state.url} 
+						onChange={this.handleUrlChange} 
+						onKeyPress={this._handleKeyPress}
+						id="jira-url"/>
 				</div>
 
 				<div className="input">
 					<p>Sprint ID:</p>
-					<input type="text" value={this.state.sprintId} onChange={this.handleSprintChange} id="jira-sprint"/>
+					<input 
+						type="text" 
+						value={this.state.sprintId} 
+						onChange={this.handleSprintChange}
+						onKeyPress={this._handleKeyPress}
+						id="jira-sprint"/>
 				</div>
 
 				<NavLink 
